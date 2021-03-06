0593ddaa0ed2a094b8b661459076521b3f079456
gameboyadvanceh <gameboyadvancehey@gmail.com>
2021-03-06 16:12:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public void TestSuppper(){
+    //
+  }
+  public static void Main (string[] args) {
+    int ten = 10;
+    float twelve = 12.5;
+    string done = "It is done!";
+    Console.WriteLine ("Hello World");
+  }
+}
